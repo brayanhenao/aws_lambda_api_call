211a19085d8ce09e6933149c9bf521037f539a96
Brayan Henao <bryanhenao96@gmail.com>
2018-10-05 23:29:56 -0500

    Final version

--- js/parcial.js
+++ js/parcial.js
@@ -5,32 +5,41 @@ function f() {
 
     let filename = file.name;
 
-    let reader = new FileReader();
+    if (!filename.includes(".txt")) {
+        f3()
+    } else {
+        let reader = new FileReader();
 
-    reader.onload = function (e) {
-        let text = e.target.result;
-        $.ajax({
-            type: "GET",
-            url: 'https://hw2m72bc78.execute-api.us-west-2.amazonaws.com/prod',
-            data: {
-                text: text,
-                lang: language,
-                filename: filename
-            },
-            dataType: 'json',
-            success(response) {
-                if (response === "Error") {
-                    f2();
-                } else {
-                    setTimeout(6000);
-                    window.location.href = "results.html";
+        reader.onload = function (e) {
+            let text = e.target.result;
+            $.ajax({
+                type: "GET",
+                url: 'https://hw2m72bc78.execute-api.us-west-2.amazonaws.com/prod',
+                data: {
+                    text: text,
+                    lang: language,
+                    filename: filename
+                },
+                dataType: 'json',
+                success(response) {
+                    if (response === "Error") {
+                        f2();
+                    } else {
+                        setTimeout(10000);
+                        window.location.href = "results.html";
+                    }
                 }
-            }
-        });
-    };
-    reader.readAsText(file);
+            });
+        };
+        reader.readAsText(file);
+    }
 }
 
 function f2() {
     window.location.href = "foreign_visitor.html";
+}
+
+function f3() {
+    $("#modal_text").empty().append("El archivo que subiste no es de un tipo soportado, solo se aceptan archivos <b><u>  .txt  </u> </b>");
+    $("#myModal").modal();
 }
